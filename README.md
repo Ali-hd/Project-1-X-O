abce5a059bf9234f6b161f9fa7c995eb15fe86fd
Ali-hd <ali.hamidaddin1@gmail.com>
2019-10-16 23:41:37 +0300

    added ReadMe and link

--- README.md
+++ README.md
@@ -1,51 +1,58 @@
 # Project Title
 
-One Paragraph of project description goes here
+Tic Tac Toe
 
 ## Description
 
-A description of your project. Explain what is the project about and what is the use for it.
+It is a 2 player game, each player get either an X or O. The player puts an X or O in a slot inside a 3x3 board that has 9 empty slots. You take turns playing and to win you must be the first player to get a full row, column or diagonal boxes of an X or O.
+
+## Game link - play it here
+
+https://ali-hd.github.io/Project-1-X-O/
 
 ### Technical Used
-What technologies you used that helped you build this project. 
 
-```
-Example:
 HTML
 CSS
 JavaScript, and jQuery for DOM manipulation
-```
 
 ### Wireframes
 
-Your step by step planning sketch of your project, that you can post them as an image in here.
+![](Wireframe_X-O.JPG)
 
 ### User Stories
 
-A story of the project user, that explains the use of the project.
-
----
+As a user I want to choose to play with X or O at the start.
+As a user I want to see the intructions.
+As a user I want to to choose who I play with.
+As a user I dont want allow a play inside a slot that has been filled.
+As a user I want to receive an alert when a player wins.
+As a user I want to see who's turn it is when playing. 
 
 ## Planning and Development Process
 
-A basic story of your planning and developing this project.
+First I started to think about how I can tell the code to know which out of the 9 slots the user clicked. I decided to use id names for each slot and when the user click I can retrieve the ID to use later on. Next, I wanted to know when a win has occurred so I made an array that contains all possible wins and used another array for each player to fill and then compare it with the wins array to check if a win has occurred. Using the sort function I was able to match it with other win options despite the order the buttons have been clicked. I didn't want the user to click a slot that has been filled so I made the click do nothing if the current click ID is included in previous player's arrays. 
 
 ### Problem-Solving Strategy
 
-What strategy did you use to solve your problems.
+I used the console.log() throughout the code, whenever I needed to check the outcome of a variable at a particular time. Also when I find a problem in my code I read the code line by line slowly to see if something doesn't make sense.
 
 ### Unsolved problems
 
-List unsolved problems which would be fixed in future iterations.
+One unsolved problem that will be fixed in futrue iteration is the player vs computer mode. A computer will fill slots that has already been filled.
+
+### Favorite functions
+
+My favorite function would be the check for win. It takes both inputs arrays after each play and sorts the move to match it with the known wins.
 
 ---
 
 ## Acknowledgments
 
-* Hat tip to anyone whose code was used
-* Inspiration
-* etc
-
----
+* I would like to thank Misk foundation & General Assembly for giving me the opportunity to take this course.  
+* And a special thanks to our instructors Yassir, Ebere, Ahmad and Atheer for helping us along the way.
 
  ## References
+
+https://developer.mozilla.org/en-US/
+https://www.w3schools.com/
--- js/main.js
+++ js/main.js
@@ -2,165 +2,178 @@ let wins = [
     [1,4,7], [2,5,8], [3,6,9], [1,2,3], [4,5,6], [7,8,9], [1,5,9], [3,5,7],[1,3,5,9], [1,3,5,7], [2,4,5,8], [2,5,6,8],
     [1,5,7,9], [1,4,5,9], [1,4,5,7],[1,5,8,9],[2,3,5,7],[3,4,5,7], [2,5,8,9], [1,2,4,7,9], [2,3,5,7,9], [1,3,5,6,7], 
     [1,2,3,4,9],[2,4,5,6], [3,5,7,9], [1,3,5,6,9], [1,5,6,9], [3,4,5,7,9], [1,3,5,8,9], [1,3,4,7,9],[1,3,5,7,9],
-    [1,4,6,7,8],[1,3,5,7,8],[2,3,5,8],[1,2,5,9],[2,5,7,8],[1,2,3,5,8],[1,2,3,6,8],[3,4,5,6,8],[2,5,6,8,9],[1,3,4,7],[3,4,5,6]]
+    [1,4,6,7,8],[1,3,5,7,8],[2,3,5,8],[1,2,5,9],[2,5,7,8],[1,2,3,5,8],[1,2,3,6,8],[3,4,5,6,8],[2,5,6,8,9],[1,3,4,7],[3,4,5,6],[1,2,4,5,7]]
 
-let p1win = [];
-let p2win = [];
-let comwin = [];
-let count = 0;
-let img = ""
-let comp;
-let rand = 0;
-let again = "";
-let opX = 0;
-let opO = 0;
-$(".container").hide()
-$(".option").hide()
-$("#reset").hide()
-$("#pick").hide()
-$("#playerX").hide()
-$("#playerO").hide()
-
-$("#p2").click(function(){
-
-    $(".choose").hide()
-    $(".option").show()
-    $("#pick").show()
-})
-$("#com").click(function(){
-    comp = 1;
-    $(".choose").hide()
-    $(".option").show()
-    $("#pick").show()
-    // $("#player1").hide()
-})
-
-$("#X").click(function(){
-    opX = 1;
-    $(".option").hide()
-    $(".container").show()
-    $("#reset").show()
-    $("header").hide()
-    $("#chose").hide()
-    $("#pick").hide()
-    $("#playerX").show()
-    $("#instructions").hide()
-})
-$("#O").click(function(){
-    opO = 1;
-    $(".option").hide()
-    $(".container").show()
-    $("#reset").show()
-    $("#chose").hide()
-    $("header").hide()
-    $("#pick").hide()
-    $("#playerY").show()
-    $("#instructions").hide()
+    let p1win = [];
+    let p2win = [];
+    let comwin = [];
+    let count = 0;
+    let img = "";
+    let comp;
+    let rand = 0;
+    let again = "";
+    let opX = 0;
+    let opO = 0;
+    let check0 = 0
+    let check1 = 0
+let check2 = 0
+    $(".container").hide();
+    $(".option").hide();
+    $("#reset").hide();
+    $("#pick").hide();
+    $("#playerX").hide();
+    $("#playerO").hide();
     
-})
-
-$(".slots").click(function(){
-    again = $(this).attr('id')//dont put move on already used slot
-    if(p1win.includes(again)==true){
-    }else if(p2win.includes(again)==true){
-    }else{
-if(comp!=1 && opX == 1){
-    if(count%2 == 0 ){p1win.push($(this).attr('id'))
-    img = $(this).attr('id')
-    $('#'+img).css("background-image", "url(../X1.png)");
-$("#playerX").hide()
-$("#playerO").show()
-}else{ p2win.push($(this).attr('id'))
-    img = $(this).attr('id')
-    $('#'+img).css("background-image", "url(../O1.png)");
-$("#playerO").hide()
-$("#playerX").show()
-        }
-count++
-check();
-}else if(comp!=1 && opO == 1){
-    if(count%2 == 0 ){p1win.push($(this).attr('id'))
-    img = $(this).attr('id')
-    $('#'+img).css("background-image", "url(../O1.png)");
-$("#playerO").hide()
-$("#playerX").show()
-}else{ p2win.push($(this).attr('id'))
-    img = $(this).attr('id')
-    $('#'+img).css("background-image", "url(../X1.png)");
-$("#playerX").hide()
-$("#playerO").show()
-        }
-count++
-check();
-}
-
-
-else{
-        
-        p1win.push($(this).attr('id'))
-        comwin.push($(this).attr('id'))
-        img = $(this).attr('id')
-        $('#'+img).css("background-image", "url(../X1.png)");
-        check();
-
-        if(count!=8){
-        console.log("this is the first comwin "+comwin)
-        rand = Math.floor(Math.random() * 9) + 1;
-        while (comwin.includes(rand)==true){
-            console.log("hey")
-            rand = Math.floor(Math.random() * 9) + 1;
+    $("#p2").click(function() {
+      $(".choose").hide();
+      $(".option").show();
+      $("#pick").show();
+    });
+    $("#com").click(function() {
+      comp = 1;
+      $(".choose").hide();
+      $(".option").show();
+      $("#pick").show();
+      // $("#player1").hide()
+    });
+    
+    $("#X").click(function() {
+      opX = 1;
+      $(".option").hide();
+      $(".container").show();
+      $("#reset").show();
+      $("header").hide();
+      $("#chose").hide();
+      $("#pick").hide();
+      $("#playerX").show();
+      $("#instructions").hide();
+    });
+    $("#O").click(function() {
+      opO = 1;
+      $(".option").hide();
+      $(".container").show();
+      $("#reset").show();
+      $("#chose").hide();
+      $("header").hide();
+      $("#pick").hide();
+      $("#playerY").show();
+      $("#instructions").hide();
+    });
+    
+    $(".slots").click(function() {
+      again = $(this).attr("id"); //dont put move on already used slot
+      if (p1win.includes(again) == true) {
+      } else if (p2win.includes(again) == true) {
+      } else {
+        if (comp != 1 && opX == 1) {
+          if (count % 2 == 0) {
+            p1win.push($(this).attr("id"));
+            img = $(this).attr("id");
+            $("#" + img).css("background-image", "url(../X1.png)");
+            $("#playerX").hide();
+            $("#playerO").show();
+          } else {
+            p2win.push($(this).attr("id"));
+            img = $(this).attr("id");
+            $("#" + img).css("background-image", "url(../O1.png)");
+            $("#playerO").hide();
+            $("#playerX").show();
           }
-          console.log("this is rand " + rand)
-        p2win.push(rand)
-        comwin.push(rand)
-        console.log("This is comwin " +comwin)
-        
-        $('#'+rand).css("background-image", "url(../O1.png)");
-            
-        count+=2
-        check();
+          count++;
+          check();
+        } else if (comp != 1 && opO == 1) {
+          if (count % 2 == 0) {
+            p1win.push($(this).attr("id"));
+            img = $(this).attr("id");
+            $("#" + img).css("background-image", "url(../O1.png)");
+            $("#playerO").hide();
+            $("#playerX").show();
+          } else {
+            p2win.push($(this).attr("id"));
+            img = $(this).attr("id");
+            $("#" + img).css("background-image", "url(../X1.png)");
+            $("#playerX").hide();
+            $("#playerO").show();
+          }
+          count++;
+          check();
+        } else {
+          p1win.push($(this).attr("id"));
+          comwin.push($(this).attr("id"));
+          img = $(this).attr("id");
+          $("#" + img).css("background-image", "url(../X1.png)");
+          check();
+    
+          if (count != 8) {
+            console.log("this is the first comwin " + comwin);
+            rand = Math.floor(Math.random() * 9) + 1;
+            while (comwin.includes(rand) == true) {
+              console.log("hey");
+              rand = Math.floor(Math.random() * 9) + 1;
+            }
+            console.log("this is rand " + rand);
+            p2win.push(rand);
+            comwin.push(rand);
+            console.log("This is comwin " + comwin);
+    
+            $("#" + rand).css("background-image", "url(../O1.png)");
+    
+            count += 2;
+            check();
+          }
         }
-        }
-    }
-});
+      }
+    });
+    
+    function check() {
+      let gameover = true;
+      for (let i = 0; i < wins.length; i++) {
 
-function check(){
-    let gameover = true;
-    for (let i = 0; i < wins.length; i++) {
-        p1win.sort(function(a, b){return a-b});
-        p2win.sort(function(a, b){return a-b});
-        if(p1win.toString() == wins[i].toString()){
-            swal.fire({title: "Player 1 is the Winner!", text: "Click to restart", type: 
-            ""}).then(function(){ 
-               location.reload();
-               }
-            );
-            gameover = false
+
+
+        p1win.sort(function(a, b) {
+          return a - b;
+        });
+        p2win.sort(function(a, b) {
+          return a - b;
+        });
+        if (p1win.toString() == wins[i].toString()) {
+          swal
+            .fire({
+              title: "Player 1 is the Winner!",
+              text: "Click to restart",
+              type: ""
+            })
+            .then(function() {
+              location.reload();
+            });
+          gameover = false;
         }
-        if(p2win.toString() == wins[i].toString()){
-            swal.fire({title: "Player 2 is the Winner!", text: "Click to restart", type: 
-            ""}).then(function(){ 
-               location.reload();
-               }
-            );
-            gameover = false
+        if (p2win.toString() == wins[i].toString()) {
+          swal
+            .fire({
+              title: "Player 2 is the Winner!",
+              text: "Click to restart",
+              type: ""
+            })
+            .then(function() {
+              location.reload();
+            });
+          gameover = false;
         }
-        if(count == 9 && gameover == true){
-            gameover = false;
-            swal.fire({title: "Game is TIE!", text: "Click to restart", type: 
-            ""}).then(function(){ 
-               location.reload();
-               }
-            );
+        if (count == 9 && gameover == true) {
+          gameover = false;
+          swal
+            .fire({ title: "Game is TIE!", text: "Click to restart", type: "" })
+            .then(function() {
+              location.reload();
+            });
         }
+      }
     }
     
-}
-
-
-
-$( "#reset" ).click(function() {
-    location.reload();
-  });
+    $("#reset").click(function() {
+      location.reload();
+    });
 
 
